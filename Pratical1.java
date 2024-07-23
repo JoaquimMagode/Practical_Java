ed70df71cfa7b75050af1c45f5eb79c2e0e8758a
Joaquim Magode <135075390+JoaquimMagode@users.noreply.github.com>
2024-07-23 11:40:51 +0530

    Practical 1
    Write a java programn to Display Hello World Message in Consolo Windows

---
+++ Pratical1.java
@@ -0,0 +1,11 @@
+/*
+Write a java programn to Display Hello World Message in Consolo Windows
+*/
+import java.util.*;
+
+public class HelloWWorld{
+
+  public static void main(String[] args){
+	System.ou.prinbyln("Hello World!");
+}
+}
